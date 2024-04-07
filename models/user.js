487e9b7206da1c0484e6a5e4408e358ee4199f8a
kplnhsyn <kplnhsyn.49@gmail.com>
2024-04-08 01:58:19 +0200

    feat:db and models added

---
+++ models/user.js
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email already exists"],
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  prompts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Prompt",
+      default: [],
+    },
+  ],
+});
+
+const User = models.User || model("User", userSchema);
+export default User;

e8d76258fb0b16806c4125b68ce62755d4276b0b
kplnhsyn <kplnhsyn.49@gmail.com>
2024-04-08 01:56:43 +0200

    feat:auth/[...search] added

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,49 @@
+import User from "@/models/user";
+import { connectToDB } from "@/utils/database";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+
+      //burrasi
+      session.user.id = sessionUser?._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        const userAlreadyExists = await User.findOne({
+          email: profile.email,
+        });
+        if (!userAlreadyExists) {
+          console.log(profile, "profile")
+          await User.create({
+            email: profile.email,
+
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+            prompts: [],
+          });
+        }
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
